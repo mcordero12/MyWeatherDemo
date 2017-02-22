92e0258b4e831a10f9fc410d3f0f9efd87517253
mcordero12 <mo.cordero12@gmail.com>
2017-02-21 19:59:40 -0500

    My Weather Demo Files - Xamarin

---
+++ MyWeather.Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("MyWeather.Droid.MainApplication, MyWeather.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md573ff0905bdb739522096341eacf2d7d2.MainApplication.class, md573ff0905bdb739522096341eacf2d7d2.MainApplication.__md_methods);
+		
+	}
+}
